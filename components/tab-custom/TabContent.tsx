787ad28bcaea7fb901fc077076fa83ab508ac602
TamTLeader <21521408@gm.uit.edu.vn>
2025-05-23 08:50:17 +0700

    fix strange bugs

--- components/tab-custom/TabContent.tsx
+++ components/tab-custom/TabContent.tsx
@@ -6,31 +6,30 @@ import { supabase } from "../../lib/supabase";
 
 const TabContent = ({ content }) => {
   const [articles, setArticles] = useState(null);
-  const [readerId, setReaderId] = useState(0)
-  const [listKey, setListKey] = useState(Date.now())
+  const [readerId, setReaderId] = useState(0);
+  const [listKey, setListKey] = useState(Date.now());
 
   const getId = async () => {
-    const { data: { user } } = await supabase.auth.getUser()
-    let { data, error } = await supabase
-      .rpc('get_id_by_email', {
-        p_email: user.email
-      })
-    if (error) console.error(error)
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) return;
+    let { data, error } = await supabase.rpc("get_id_by_email", {
+      p_email: user.email,
+    });
+    if (error) console.error(error);
     else {
       setReaderId(data);
     }
-  }
+  };
 
   async function fetchData() {
-    await getId()
-    let { data, error } = await supabase.rpc(
-      "get_article_list_from_category",
-      {
-        category_id: content?.id,
-        user_id: readerId,
-      }
-    );
-
+    await getId();
+    let { data, error } = await supabase.rpc("get_article_list_from_category", {
+      category_id: content?.id,
+      user_id: readerId,
+    });
+    console.log(data, error);
     if (error) console.error(error);
     else {
       setArticles(data);
@@ -42,12 +41,18 @@ const TabContent = ({ content }) => {
   }, [readerId]);
 
   useEffect(() => {
-    getId()
-  }, [])
+    getId();
+  }, []);
 
   return (
     <View style={styles.container}>
-      {articles && <BigArticleList key={listKey} articles={articles} scrollEnabled={true} />}
+      {articles && (
+        <BigArticleList
+          key={listKey}
+          articles={articles}
+          scrollEnabled={true}
+        />
+      )}
     </View>
   );
 };
